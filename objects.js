a60ceebacb0ab02f1f365510ab10410833fa4726
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-10-12 15:51:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
-var playlist = { artist names: 'song titles'}
+var playlist = { artistName: 'songTitle'}
 
-var playlist = new Object({ artist names: 'Song titles'});
+var playlist = new Object({ artistNames: 'songTitles'});
 
-function updatePlaylist(playlist, 'artist name', song title) {
- playlist[artist names] = 'song titles'
+function updatePlaylist(playlist, artistName, songTitles) {
+ playlist[artistName] = 'songTitles'
   return playlist
 }
 
@@ -11,4 +11,30 @@ Object.assign({playlist}, {})
 function removeFromPlaylist( the playlist, 'artist name', song title) {
   return Object.assign({}, obj, {[Key]: value})
 }
-playlist.
+
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+
+  describe('removeFromPlaylist(playlist, artistName)', () => {
+    it('removes `artistName` from `playlist`', () => {
+      removeFromPlaylist(playlist, 'Slowdive')
+
+      expect(playlist).
+        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+
+      expect(playlist).
+        not.to.have.all.keys({'Slowdive': 'Alison'})
+    })
+  })
+})
