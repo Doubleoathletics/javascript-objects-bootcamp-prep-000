afbfeccd31df04e4c2613708ccc4ad2ef29d182a
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-10-12 15:32:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,12 @@
-var playlist = {};
-var playlist = new Object({ artist names: 'Song titles'})
+var playlist 
 
+var playlist = new Object({ artist names: 'Song titles'})
+function updatePlaylist{
+  obj[artist names] = song titles
+  return obj
+}
+
+Object.assign({}, {})
 function updatePlaylist( the playlist, 'artist name', song title) {
-  return Object.assight({}, obj, {[Key]: value})
+  return Object.assign({}, obj, {[Key]: value})
 }
