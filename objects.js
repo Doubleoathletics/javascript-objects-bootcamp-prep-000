d8ad3390377f8371dd6f38cc40bfdd20a9b6bc8a
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-10-12 15:59:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,24 +7,13 @@ function updatePlaylist(playlist, artistName, songTitles) {
   return playlist
 }
 
-Object.assign({playlist}, {})
-function removeFromPlaylist( the playlist, 'artist name', song title) {
+Object.assign({playlist}, {});
+
+function removeFromPlaylist(playlist, 'artistName') {
+delete playlist.artistNames;
   return Object.assign({}, obj, {[Key]: value})
 }
 
-  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-    before(() => {
-      playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-    })
-
-    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
-
-      expect(playlist).
-        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-    })
-  })
 
   describe('removeFromPlaylist(playlist, artistName)', () => {
     it('removes `artistName` from `playlist`', () => {
