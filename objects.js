a7123df95eda02ec8885f41b67425ef5d6d37aac
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-10-12 16:20:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(playlist, artistName, songTitles) {
 function removeFromPlaylist(playlist, artistName) {
 Object.assign({}, playlist, {artistName})
 delete playlist.artistName;
-  return playlist
+  return Object.assign
 }
 
 
